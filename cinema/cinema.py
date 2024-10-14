504d357531fdc507bac2c00092bef1b233511c8d
Negrov <galimovgal@mail.ru>
2024-10-14 12:32:12 +0500

    little modifiec

--- cinema/cinema.py
+++ cinema/cinema.py
@@ -12,9 +12,17 @@ class Cinema:
     def append(self, room):
         self.room.append(room)
 
+    @property
+    def _to_dict_room(self):
+        result = list()
+        for room in self.rooms:
+            result.append(room.to_dict_())
+        return result
+
     def wright_json(self):
-        with open('db_cinema.json', 'w') as db:
-            cinema_dict = {'name': self.name, 'rooms': self.rooms}
+        # with open('cinema\\db_cinema.json', 'w') as db:
+        with open('C:\\Users\\Negrov\\PycharmProjects\\pythonProject3\\cinema\\db_cinema.json', 'w') as db:
+            cinema_dict = {'name': self.name, 'rooms': self._to_dict_room}
             json.dump(cinema_dict, db)
 
     def __str__(self):
@@ -28,16 +36,25 @@ class Room:
         self.height = 0
         self.seats = list()
 
+    def __len__(self):
+        return len(self.seats)
+
+    def to_dict_(self):
+        return {'name': self.name, 'weight': self.weight, 'height': self.height, 'seats': self.seats}
+
+    def len_sub(self, row):
+        return len(self.seats[row])
+
     def generated_seat(self, weight, height):
         self.weight = weight
         self.height = height
-        self.seats = [[' ' for _ in range(weight)] for _ in range(height)]
+        self.seats = [['__' for _ in range(weight)] for _ in range(height)]
 
     def get_seat(self):
         return self.seats
 
     def refact_seat(self, weight, height):
-        self.seats[weight][height] = '#'
+        self.seats[weight][height] = '()' if self.seats[weight][height] == '__' else '__'
 
     def __str__(self):
         return self.name
--- cinema/root_menu.py
+++ cinema/root_menu.py
@@ -3,28 +3,70 @@ from os import system
 from cinema import Cinema, Room
 
 
+def format_num(num):
+    return f'{num: >2}'
+
+def create_choice(room, choice_row):
+    result = [f'{str(i + 1): >3}) {room.get_seat()[choice_row][i]}' for i in range(room.len_sub(choice_row))]
+    return result
+
 def join_cinema():
         system('cls')
         name = input('Название кинотеатра:\n')
         rooms = input('Нипишите залы (формат ввода: Название1, Название2, Название3 и т.д\n')
-        while rooms.find(', ') == -1:
+        while (rooms.find(', ') == -1 or  len(rooms) == 0) and not rooms.isdigit():
+            system('cls')
             print('Неверный ввод залов.')
             rooms = input('Нипишите залы (формат ввода: Название1, Название2, Название3 и т.д\n')
         rooms = sorted(set(rooms.strip().split(', ')))
-        for room in rooms:
-            if not("зал" in room):
-                room += ' зал'
         cinema = Cinema(name, rooms)
         for room in cinema.get_rooms():
             system('cls')
-            print(room)
+            print(room, 'зал')
             weight, height = input('Ввидите размеры прямоугольника в котором расположены кресла\n'
                                    '(в формате: кресла по горизонтали X кресла по вертикали)\n').lower().split(' x ')
             room.generated_seat(int(weight), int(height))
-            print(*room.get_seat(), sep='\n')
-            input('Продолить')
+
+            while True:
+                weight = int(weight)
+                system('cls')
+                room = room
+                print(f'Заполните имеющиеся места в зале {str(room)}:')
+                questions_big = [
+                    inquirer.List(
+                        name='seats', message=f'{" " * 4}{" ".join([format_num(i) for i in range(1, weight + 1)])}',
+                        choices=([f'{str(len(room) - i): >3}) {" ".join(room.get_seat()[i])}' for i in range(len(room))]
+                                + ['Выход'])
+
+                    )
+                ]
+                choice = inquirer.prompt(questions_big)['seats']
+                if choice == 'Выход':
+                    break
+                row = int(choice[:choice.find(')')])
+                choice_row = len(room) - row
+
+                #_______________места__________________
+                room = room
+                choice_row = choice_row
+                system('cls')
+                print(row, 'ряд')
+                questions_small = [
+                    inquirer.Checkbox(
+                        'place', message='Выберете место',
+                        choices=create_choice(room, choice_row)
+                    )
+                ]
+                choice = inquirer.prompt(questions_small)['place']
+                for check in choice:
+                    col = int(check[:check.find(')')])
+                    choice_col = col - 1
+                    room.refact_seat(choice_row, choice_col)
+
         cinema.wright_json()
 
+def refact_cimina():
+    pass
 
 
 def root_menu():
